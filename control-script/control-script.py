396e0b6652d3ef609b1d07f0d6775b0cf9dcbc36
Koki Yoshida <kyoshida@MacBook-Pro.local>
2015-06-29 10:52:40 -0700

    Finished spawning subprocesses

--- control-script/control-script.py
+++ control-script/control-script.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import subprocess
+import os, sys
+from subprocess import Popen, PIPE, STDOUT
+
+cmd1 = "source ../../msisdn-cli/.venv/bin/activate"
+cmd2 = "msisdn-cli -H https://msisdn.services.mozilla.com -c 310"
+child_process = subprocess.Popen("{}; {}".format(cmd1, cmd2), stdin=PIPE, stdout=None, stderr=None, shell=True)
+child_process.communicate("+16507399259")
+
 
