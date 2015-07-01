83dfcab0b8f4fcf565cc00b1b4270d8b054d08cf
Koki Yoshida <kyoshida@MacBook-Pro.local>
2015-07-01 13:51:00 -0700

    semi-automated msisdn-gateway test

--- control-script/control-script.py
+++ control-script/control-script.py
@@ -1,26 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import subprocess
-import os, sys
-from subprocess import Popen, PIPE, STDOUT
-
 import pexpect
 
-virtualenv_command = "source ../../msisdn-cli/.venv/bin/activate"
 worker_host = "msisdn-cli -H https://msisdn.services.mozilla.com"
-# child_process = subprocess.Popen("{}; {}".format(cmd1, cmd2), stdin=PIPE, stdout=None, stderr=None, shell=True)
-# child_process.communicate("+16507399259")
 
 # spawn a worker for msisdn-cli
-worker = pexpect.spawn("bash")
 msisdn_command = worker_host + " -c 310 -n +16507399259"
-print "Executing command: " + msisdn_command
-print worker.before
-worker.sendline(msisdn_command)
-print "msisdn_command sent"
-print worker.before
-worker.expect("Please\senter\sthe\scode\sthat", timeout=4)
-print "Code sent"
+worker = pexpect.spawn(msisdn_command)
+print("Executing command: " + msisdn_command)
+worker.expect("Please\senter\sthe\scode\sthat")
 worker.interact()
-
