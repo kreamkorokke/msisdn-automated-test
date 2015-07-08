b11c4ed3e75ec2eb32c8ce21dcbe4e258cb542f3
Koki Yoshida <kyoshida@MacBook-Pro.local>
2015-07-08 11:37:52 -0700

    Seperated install.sh and run.sh

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+
+REQUIREMENTS = [
+    'fabric',
+    'twilio',
+    'pexpect'
+]
+
+setup(
+    name='msisdn-automated-test',
+    version='1.0.0',
+    description='msisdn-automated-test',
+    long_description=README,
+    author='Koki Yoshida',
+    author_email='kreamkorokke@gmail.com',
+    url='https://github.com/kreamkorokke/msisdn-automated-test',
+    license="MIT",
+    install_requires=REQUIREMENTS,
+    keywords=['msisdn-automated-test', 'setup.py'],
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+    ],
+)
