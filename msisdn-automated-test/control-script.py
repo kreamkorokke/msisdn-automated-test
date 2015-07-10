520f1f873ca77c30045704d61de5552b4652b126
Koki Yoshida <kyoshida@MacBook-Pro.local>
2015-07-10 15:41:23 -0700

    deleted auth tokens

--- msisdn-automated-test/control-script.py
+++ msisdn-automated-test/control-script.py
@@ -6,34 +6,29 @@ import time
 import sys
 import re
 import argparse
+import json
 from twilio.rest import TwilioRestClient
 
 host = "https://msisdn.services.mozilla.com"
 host_number = "+12182967993"
-mccs = ["310"]
-numbers = ["+16502650808"]
-account_sid = "ACea6b158b0109812803f02e90e026fa0c"
-auth_token = "b6c0cf4ac5b0c28b681a490b16f352a2"
-client = TwilioRestClient(account_sid, auth_token)
 
-def clean_up_message_logs():
+
+def clean_up_message_logs(client):
 	# clean up the previous message logs
 	messages = client.messages.list()
 	for i in range(0, len(messages)):
 		client.messages.delete(messages[i].sid)
 
-def run_msisdn_cli(cli_command, gateway_number, test_number):
+def run_msisdn_cli(client, cli_command, gateway_number, test_number):
 	# spawn a worker for msisdn-cli
 	worker = pexpect.spawn("bash")
 	worker.logfile = sys.stdout
 	worker.sendline("cd ./msisdn-cli; virtualenv .venv; . .venv/bin/activate; python ./setup.py develop")
 	worker.sendline(cli_command)
-	# worker = pexpect.spawn(cli_command)
 	print("Executing command: " + cli_command)
 
 	# retrieve the verification message
 	worker.expect("Please\senter\sthe\scode\sthat\syou\swill\sget\sby\sSMS\sfrom\s")
-	# print(worker.before)
 	message_log = client.messages.list(gateway_number, test_number, time.strftime("%Y-%m-%d", time.gmtime()))
 	while not message_log:
 		time.sleep(1)
@@ -44,15 +39,32 @@ def run_msisdn_cli(cli_command, gateway_number, test_number):
 	match = re.search("^Your\sverification\scode\sis:\s(\d\d\d\d\d\d)", verification_message)
 	worker.sendline(match.group(1))
 	worker.expect("Verified")
-	# print(worker.before)
 	print("++++++++++++++++++++ Number: " + test_number + " VERIFIED +++++++++++++++++++")
 
 def main():
-	parser = argparse.ArgumentParser(description='Scripts for automating msisdn-gateway test', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser = argparse.ArgumentParser(
+		description='Scripts for automating msisdn-gateway test', 
+		formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-	clean_up_message_logs()
-	command = "msisdn-cli -H " + host + " -c " + mccs[0] + " -n " + numbers[0]
-	run_msisdn_cli(command, host_number, numbers[0])
+	parser.add_argument(
+		"-a", "--account-auth", 
+		required=True)
+
+	parser.add_argument(
+		"-n", "--numbers",
+		required=True)
+
+	args = vars(parser.parse_args())
+
+	with open(args["account_auth"]) as account_auth_file:
+		account_auth = json.load(account_auth_file)
+	with open(args["numbers"]) as numbers_file:
+		numbers = json.load(numbers_file)
+
+	client = TwilioRestClient(account_auth["account_sid"], account_auth["auth_token"])
+	clean_up_message_logs(client)
+	command = "msisdn-cli -H " + host + " -c " + numbers["mcc"] + " -n " + numbers["number"]
+	run_msisdn_cli(client, command, host_number, numbers["number"])
 
 
 if __name__ == "__main__":
--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 fab setup || exit 1
 echo "-----------------------------------Setup Complete----------------------------------"
 echo "---------------------------Virtual Environment Activated---------------------------"
-python ./msisdn-automated-test/control-script.py
+python ./msisdn-automated-test/control-script.py -a ./account_auth.json -n ./numbers.json
 echo "------------------------------------Completed Test---------------------------------"
 echo "------------------------------------Begin Teardown---------------------------------"
 fab teardown || exit 1
